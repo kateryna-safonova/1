01c4196a77593d241c14ae0ab435cb9b78d546e3
kateryna-safonova <98874862+kateryna-safonova@users.noreply.github.com>
2022-12-02 11:15:59 +0100

    Add files via upload

---
+++ 1/c++/c++/main.cpp
@@ -0,0 +1,45 @@
+//#include <iostream>
+//#include <iomanip>
+//using namespace std;
+//int main()
+//{
+//    float a,b,pole;
+//    cout << "Program oblicza pole prostokąta." << endl;
+//    cout << "Podaj bok a." << endl;
+//    cin >>a;
+//    cout << "Podaj bok b." << endl;
+//    cin >> b;
+//    pole = a*b;
+//    cout << fixed;
+//    cout << setprecision(2);
+//    cout << "Pole prostokata o boku a = " << a << "i boku b =" << b;
+//    cout << "wynosi" << pole << "." << endl;
+//    return 0;
+//}
+//#include <iostream>
+//#include <iomanip>
+//int main()
+//{
+//    int a,b;
+//    float pole;
+//    printf("Program oblicza pole prostokata");
+//    printf("\nPodaj bok a.");
+//    scanf("&a");
+//    printf("\nPodaj bok b.");
+//    scanf("&b");
+//    pole = a*b;
+//    printf("\nPole prostokata o boku a = &a i boku b = &b wynosi %10.2E",a,b,pole);
+//    return 0;
+//
+//}
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+using namespace std;
+int main()
+{
+    cout << setprecision(2);
+    cout << sqrt(M_PI);
+
+}
+
